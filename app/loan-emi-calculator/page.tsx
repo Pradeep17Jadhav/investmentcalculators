d08f7421c0fbbeb608930dad24ddc707b8030ce2
Pradeep Jadhav <pradeep17jadhav@gmail.com>
2025-03-21 20:13:57 +0530

    update metadata for load calculator

--- app/loan-emi-calculator/page.tsx
+++ app/loan-emi-calculator/page.tsx
@@ -1,11 +1,20 @@
 import LoanPage from "./LoanPage";
 
+const title =
+  "Loan EMI Calculator - Calculate EMI for Home, Personal & Car Loans";
+const description =
+  "Calculate monthly EMI for various loans like home, personal, and car loans using our EMI calculator. Plan your loan repayment schedule efficiently.";
+const keywords =
+  "loan EMI calculator, calculate loan EMI, home loan EMI, personal loan EMI, car loan EMI, loan repayment plan, best loan rates 2025";
+
 export const metadata = {
-  title: "Loan EMI Calculator - Calculate EMI for Home, Personal & Car Loans",
-  description:
-    "Calculate monthly EMI for various loans like home, personal, and car loans using our EMI calculator. Plan your loan repayment schedule efficiently.",
-  keywords:
-    "loan EMI calculator, calculate loan EMI, home loan EMI, personal loan EMI, car loan EMI, loan repayment plan, best loan rates 2025",
+  title,
+  description,
+  keywords,
+  openGraph: {
+    description,
+    keywords,
+  },
 };
 
 export default LoanPage;
