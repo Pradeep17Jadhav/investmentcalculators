ed1c1d4bf8022ae3f2f79b718bda9e4796894d7f
Pradeep Jadhav <pradeep17jadhav@gmail.com>
2025-04-02 21:55:59 +0530

    move comments out

--- app/blog/[blogUrl]/BlogPage.tsx
+++ app/blog/[blogUrl]/BlogPage.tsx
@@ -34,46 +34,48 @@ const BlogPage = async ({ metadata, content }: Props) => {
   const readTime = metadata.readTime;
 
   return (
-    <div className={styles.container}>
-      <Grid container spacing={4}>
-        <Grid item sm={8} xs={12}>
-          <div className={styles.leftSection}>
-            <div className={styles.title}>
-              <h1>{title}</h1>
-            </div>
-            <div className={styles.blogInfo}>
-              <div className={styles.blogInfoItem}>
-                <CalendarTodayIcon className={styles.icon} fontSize="small" />
-                {formatDate(date)}
+    <>
+      <div className={styles.container}>
+        <Grid container spacing={4}>
+          <Grid item sm={8} xs={12}>
+            <div className={styles.leftSection}>
+              <div className={styles.title}>
+                <h1>{title}</h1>
               </div>
-              <div className={styles.blogInfoItem}>
-                <PersonIcon className={styles.icon} fontSize="small" />
-                <div>{author}</div>
+              <div className={styles.blogInfo}>
+                <div className={styles.blogInfoItem}>
+                  <CalendarTodayIcon className={styles.icon} fontSize="small" />
+                  {formatDate(date)}
+                </div>
+                <div className={styles.blogInfoItem}>
+                  <PersonIcon className={styles.icon} fontSize="small" />
+                  <div>{author}</div>
+                </div>
+                <div className={styles.blogInfoItem}>
+                  <AccessTimeIcon className={styles.icon} fontSize="small" />
+                  <div>{`${readTime} min read`}</div>
+                </div>
               </div>
-              <div className={styles.blogInfoItem}>
-                <AccessTimeIcon className={styles.icon} fontSize="small" />
-                <div>{`${readTime} min read`}</div>
-              </div>
-            </div>
-            <hr />
+              <hr />
 
-            {!!image && (
-              <div className={styles.imageContainer}>
-                <Image className={styles.img} src={image} alt={title} fill />
-              </div>
-            )}
-            <div className={styles.content}>{renderableContent}</div>
-          </div>
+              {!!image && (
+                <div className={styles.imageContainer}>
+                  <Image className={styles.img} src={image} alt={title} fill />
+                </div>
+              )}
+              <div className={styles.content}>{renderableContent}</div>
+            </div>
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <div className={styles.rightSection}>
+              <SidebarLists title="Related Blogs" blogs={relatedBlogs} />
+              <SidebarCards title="Latest Blogs" blogs={recentBlogs} />
+            </div>
+          </Grid>
         </Grid>
-        <Grid item sm={4} xs={12}>
-          <div className={styles.rightSection}>
-            <SidebarLists title="Related Blogs" blogs={relatedBlogs} />
-            <SidebarCards title="Latest Blogs" blogs={recentBlogs} />
-          </div>
-        </Grid>
-      </Grid>
+      </div>
       <DisqusComments url={absoluteUrl} title={title} id={metadata.slug} />
-    </div>
+    </>
   );
 };
 
